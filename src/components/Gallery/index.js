252731d706bcc25470d4533368c77a85c77f1a54
vayne <miguel.osorio19@yahoo.com>
2022-03-23 17:36:07 -0500

    added prop for gallery component, rendering photo via category

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,20 +1,15 @@
 import React from "react";
+import Photolist from "../Photolist";
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg';
 
-function Gallery(props) {
-    const currentCategory = {
-        name: "commercial",
-        description: "Photos of grocery stores, food trucks, and other commercial projects",
-    };
+function Gallery({ currentCategory }) {
+    const { name, description } = currentCategory;
 
     return (
         <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.name}</p>
-            <div>
-                <img src={photo} alt="Commercial Example" className="img-thumbnail mx-1"/>
-            </div>
+            <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+            <Photolist category={currentCategory.name} />
         </section>
     );
 }
